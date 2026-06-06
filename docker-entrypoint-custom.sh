bcb64b8589e5714277085a06027376e1dea54919
Ant Skelton <ant@ant.org>
2026-06-07 00:16:41 +0100

    copy theme dir, don't symlink it (or ghost complains)

--- docker-entrypoint-custom.sh
+++ docker-entrypoint-custom.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-# Ghost symlinks only its built-in themes into the content volume; add ours.
 mkdir -p /var/lib/ghost/content/themes
-ln -sf /var/lib/ghost/current/content/themes/source-custom \
-       /var/lib/ghost/content/themes/source-custom
+rm -rf /var/lib/ghost/content/themes/source-custom
+cp -r /var/lib/ghost/current/content/themes/source-custom \
+      /var/lib/ghost/content/themes/source-custom
 exec docker-entrypoint.sh "$@"
